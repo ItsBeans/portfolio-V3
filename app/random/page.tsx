63679bf2a0ec50eab15ee772160eae68b748fd1f
ItsBeans <bennyrocys@gmail.com>
2025-05-16 21:45:33 +0100

    added cat to random page

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -4,20 +4,27 @@ import ChessStatsCard from "../components/ChessStatsCard";
 import NowPlaying from "../components/NowPlaying";
 
 export default function Random() {
-    return (
-        <>
-        <Header/>
-        <div className="max-w-3xl mx-auto p-8">
-      <h1 className="text-6xl font-bold text-black dark:text-white italic mb-12">
-        random
-      </h1>
-      <p className="text-lg text-gray-600 dark:text-gray-300 mb-8">random collection of random things </p>
-      <div className="flex flex-row space-x-6 items-start">
-        <ChessStatsCard username="ItsBeans" />
-        <NowPlaying />
+  return (
+    <>
+      <Header />
+      <div className="max-w-5xl mx-auto p-8">
+        <h1 className="text-6xl font-bold text-black dark:text-white italic mb-12">
+          random
+        </h1>
+        <p className="text-black dark:text-white font-medium mb-4">some random things</p>
+        <div className="grid grid-cols-1 md:grid-cols-3 items-start">
+          <div className="space-y-6">
+            <NowPlaying />
+          </div>
+            <img
+              src="/raon.jpeg"
+              alt="Raon"
+              className="mx-auto rounded-2xl shadow-md w-full max-w-sm"
+            />
         </div>
-    </div>
-    <DockDemo/>
-      </>
-    );
-  }
+      </div>
+      <DockDemo />
+    </>
+  );
+}
+
